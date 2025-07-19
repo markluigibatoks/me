4cc3466b525b7f798493c36a52b26fd339f61c29
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-19 00:12:12 -0230

    refactor: add images/front-page directory

---
+++ page-about.php
@@ -0,0 +1,8 @@
+<?php 
+/*  Template Name: About Page
+    Template Post Type: page
+*/
+  get_header();
+  get_template_part("/partials/abous-us/section1");
+  get_footer();
+?>
---
+++ partials/about-us/section1.php
@@ -0,0 +1,10 @@
+<div class="main-box lg:py-32 py-10">
+    <div class="wrapper">
+        <div class="w-full text-center">
+            <span>Subheading</span>
+            <h2>About Us</h2>
+            <p>We are a company that values excellence and innovation.</p>
+            <a href="#" class="button-primary mx-auto lg:mx-0">Learn More</a>
+        </div>
+    </div>
+</div>
