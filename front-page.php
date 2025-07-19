132ca0c8c09dae4118df2352fa02601d3f85163e
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-18 23:06:19 -0230

    refactor: change directory from 'partials/' to 'partials/front-page'

--- front-page.php
+++ front-page.php
@@ -1,8 +1,8 @@
 <?php
     get_header();
-    get_template_part("/partials/banner");
-    get_template_part("/partials/middle1");
-    get_template_part("/partials/main");
-    get_template_part("/partials/bottom1");
+    get_template_part("/partials/front-page/banner");
+    get_template_part("/partials/front-page/middle1");
+    get_template_part("/partials/front-page/main");
+    get_template_part("/partials/front-page/bottom1");
     get_footer();
 ?>
