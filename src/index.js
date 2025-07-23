b6b72c3df0fd534a1bb4f89ce11887e76bbea4ed
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-23 13:36:28 -0230

    refactor: delete unused code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Drawer from "./scripts/Drawer"
 import QuickHacks from "./scripts/QuickHacks"
 import FormContactUs from "./scripts/FormContactUs";
-import FormNewsletter from "./scripts/FormNewsletter";
 
 document.addEventListener('DOMContentLoaded', function () {
   new Drawer('#drawer', '#drawer-toggle', '#drawer-close')
@@ -10,10 +9,5 @@ document.addEventListener('DOMContentLoaded', function () {
   const contactUsForm = document.querySelector('#contact-us-form');
   if (contactUsForm) {
     new FormContactUs('#contact-us-form');
-  }
-
-  const newsletterForm = document.querySelector('#newsletter-form');
-  if (newsletterForm) {
-    new FormNewsletter('#newsletter-form');
   }
 });
